c16ede834b592658a17826406bfb700dc91d0d1e
LiuZach <24520575@qq.com>
2016-06-20 18:58:51 +0800

    no message

--- InsuranceBroker/View/Global/EditAbleTextView.m
+++ InsuranceBroker/View/Global/EditAbleTextView.m
@@ -10,33 +10,33 @@
 
 @implementation EditAbleTextView
 
-- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
-{
-    if (action == @selector(paste:)) {
-        return YES;
-    }else{
-        return [super canPerformAction:action withSender:sender];
-    }
-}
-
-- (BOOL)canBecomeFirstResponder
-{
-    return YES;
-}
-
-- (BOOL)becomeFirstResponder
-{
-    if ([super becomeFirstResponder]) {
-        return YES;
-    }
-    return NO;
-}
-
-- (void)paste:(id)sender
-{
-    UIPasteboard *board = [UIPasteboard generalPasteboard];
-    self.text = board.string;
-    [self resignFirstResponder];
-}
+//- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+//{
+//    if (action == @selector(paste:)) {
+//        return YES;
+//    }else{
+//        return [super canPerformAction:action withSender:sender];
+//    }
+//}
+//
+//- (BOOL)canBecomeFirstResponder
+//{
+//    return YES;
+//}
+//
+//- (BOOL)becomeFirstResponder
+//{
+//    if ([super becomeFirstResponder]) {
+//        return YES;
+//    }
+//    return NO;
+//}
+//
+//- (void)paste:(id)sender
+//{
+//    UIPasteboard *board = [UIPasteboard generalPasteboard];
+//    self.text = board.string;
+//    [self resignFirstResponder];
+//}
 
 @end
